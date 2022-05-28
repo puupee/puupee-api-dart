63a1fc10d72f2adbc0b577e4bf1f3747aff5ea90
jiejie-dev <jeremaihloo1024@gmail.com>
2022-05-28 14:34:15 +0800

    update

--- doggy/doc/MemberDto.md
+++ doggy/doc/MemberDto.md
@@ -9,7 +9,15 @@ import 'package:doggy_api/api.dart';
 Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
 **expiredAt** | [**DateTime**](DateTime.md) |  | [optional] 
+**todoExpiredAt** | [**DateTime**](DateTime.md) |  | [optional] 
+**noteExpiredAt** | [**DateTime**](DateTime.md) |  | [optional] 
+**albumExpiredAt** | [**DateTime**](DateTime.md) |  | [optional] 
+**fileExpiredAt** | [**DateTime**](DateTime.md) |  | [optional] 
 **level** | [**MemberLevel**](MemberLevel.md) |  | [optional] 
+**todoLevel** | [**MemberLevel**](MemberLevel.md) |  | [optional] 
+**noteLevel** | [**MemberLevel**](MemberLevel.md) |  | [optional] 
+**albumLevel** | [**MemberLevel**](MemberLevel.md) |  | [optional] 
+**fileLevel** | [**MemberLevel**](MemberLevel.md) |  | [optional] 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
 
--- doggy/lib/model/member_dto.dart
+++ doggy/lib/model/member_dto.dart
@@ -14,34 +14,98 @@ class MemberDto {
   /// Returns a new [MemberDto] instance.
   MemberDto({
     this.expiredAt,
+    this.todoExpiredAt,
+    this.noteExpiredAt,
+    this.albumExpiredAt,
+    this.fileExpiredAt,
     this.level,
+    this.todoLevel,
+    this.noteLevel,
+    this.albumLevel,
+    this.fileLevel,
   });
 
 
   DateTime? expiredAt;
 
+  DateTime? todoExpiredAt;
+
+  DateTime? noteExpiredAt;
+
+  DateTime? albumExpiredAt;
+
+  DateTime? fileExpiredAt;
+
   MemberLevel? level;
+
+  MemberLevel? todoLevel;
+
+  MemberLevel? noteLevel;
+
+  MemberLevel? albumLevel;
+
+  MemberLevel? fileLevel;
 
   @override
   bool operator ==(Object other) => identical(this, other) || other is MemberDto &&
      other.expiredAt == expiredAt &&
-     other.level == level;
+     other.todoExpiredAt == todoExpiredAt &&
+     other.noteExpiredAt == noteExpiredAt &&
+     other.albumExpiredAt == albumExpiredAt &&
+     other.fileExpiredAt == fileExpiredAt &&
+     other.level == level &&
+     other.todoLevel == todoLevel &&
+     other.noteLevel == noteLevel &&
+     other.albumLevel == albumLevel &&
+     other.fileLevel == fileLevel;
 
   @override
   int get hashCode =>
     expiredAt.hashCode +
-    level.hashCode;
+    todoExpiredAt.hashCode +
+    noteExpiredAt.hashCode +
+    albumExpiredAt.hashCode +
+    fileExpiredAt.hashCode +
+    level.hashCode +
+    todoLevel.hashCode +
+    noteLevel.hashCode +
+    albumLevel.hashCode +
+    fileLevel.hashCode;
 
   @override
-  String toString() => 'MemberDto[expiredAt=$expiredAt, level=$level]';
+  String toString() => 'MemberDto[expiredAt=$expiredAt, todoExpiredAt=$todoExpiredAt, noteExpiredAt=$noteExpiredAt, albumExpiredAt=$albumExpiredAt, fileExpiredAt=$fileExpiredAt, level=$level, todoLevel=$todoLevel, noteLevel=$noteLevel, albumLevel=$albumLevel, fileLevel=$fileLevel]';
 
   Map<String, dynamic> toJson() {
     final json = <String, dynamic>{};
     if (expiredAt != null) {
       json[r'expiredAt'] = expiredAt!.toUtc().toIso8601String();
     }
+    if (todoExpiredAt != null) {
+      json[r'todoExpiredAt'] = todoExpiredAt!.toUtc().toIso8601String();
+    }
+    if (noteExpiredAt != null) {
+      json[r'noteExpiredAt'] = noteExpiredAt!.toUtc().toIso8601String();
+    }
+    if (albumExpiredAt != null) {
+      json[r'albumExpiredAt'] = albumExpiredAt!.toUtc().toIso8601String();
+    }
+    if (fileExpiredAt != null) {
+      json[r'fileExpiredAt'] = fileExpiredAt!.toUtc().toIso8601String();
+    }
     if (level != null) {
       json[r'level'] = level;
+    }
+    if (todoLevel != null) {
+      json[r'todoLevel'] = todoLevel;
+    }
+    if (noteLevel != null) {
+      json[r'noteLevel'] = noteLevel;
+    }
+    if (albumLevel != null) {
+      json[r'albumLevel'] = albumLevel;
+    }
+    if (fileLevel != null) {
+      json[r'fileLevel'] = fileLevel;
     }
     return json;
   }
@@ -51,7 +115,15 @@ class MemberDto {
   // ignore: prefer_constructors_over_static_methods
   static MemberDto fromJson(Map<String, dynamic> json) => MemberDto(
         expiredAt: json[r'expiredAt'] == null ? null : mapDateTime(json, r'expiredAt', ''),
+        todoExpiredAt: json[r'todoExpiredAt'] == null ? null : mapDateTime(json, r'todoExpiredAt', ''),
+        noteExpiredAt: json[r'noteExpiredAt'] == null ? null : mapDateTime(json, r'noteExpiredAt', ''),
+        albumExpiredAt: json[r'albumExpiredAt'] == null ? null : mapDateTime(json, r'albumExpiredAt', ''),
+        fileExpiredAt: json[r'fileExpiredAt'] == null ? null : mapDateTime(json, r'fileExpiredAt', ''),
         level: json[r'level'] == null ? null : MemberLevel.fromJson(json[r'level']),
+        todoLevel: json[r'todoLevel'] == null ? null : MemberLevel.fromJson(json[r'todoLevel']),
+        noteLevel: json[r'noteLevel'] == null ? null : MemberLevel.fromJson(json[r'noteLevel']),
+        albumLevel: json[r'albumLevel'] == null ? null : MemberLevel.fromJson(json[r'albumLevel']),
+        fileLevel: json[r'fileLevel'] == null ? null : MemberLevel.fromJson(json[r'fileLevel']),
     );
 
   static List<MemberDto> listFromJson(List json, {bool? growable,}) =>
